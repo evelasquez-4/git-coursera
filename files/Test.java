ba7c45cf3a1adbdcebddad0ba0f073d44759872e
evelasquez <raven@ravens-MacBook-Air.local>
2020-10-20 23:37:43 -0400

    function added from B side

--- files/Test.java
+++ files/Test.java
@@ -2,4 +2,7 @@ class Test{
 	public static void main(String[] args){
 		System.out.println("Message from main repository");
 	}
+	public static String messageSideB(String message){
+		System.out.println(message);
+	}
 }
