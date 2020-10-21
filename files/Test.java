0750faf6c681adde8f8633226760e65d03093530
evelasquez <raven@ravens-MacBook-Air.local>
2020-10-20 23:57:05 -0400

    commit from b side

--- files/Test.java
+++ files/Test.java
@@ -3,7 +3,8 @@ class Test{
 		System.out.println("Message from main repository");
 	}
 	public static String messageSideB(String message){
-
+		System.out.println(message);
+	}
 	public static String messageSideA(String message){
 		System.out.println(message);
 	}
